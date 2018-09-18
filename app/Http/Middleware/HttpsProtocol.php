d87903246028e1635ab6c27a04272a0ef2614504
Nikita <Nkmelnikov@hotmail.com>
2018-09-18 12:27:50 +0300

    https test

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Request;
+
+class HttpsProtocol
+{
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		if (!app()->environment('local')) {
+			// for Proxies
+			Request::setTrustedProxies([$request->getClientIp()]);
+
+			if (!$request->isSecure()) {
+				return redirect()->secure($request->getRequestUri());
+			}
+		}
+
+		return $next($request);
+	}
+}
